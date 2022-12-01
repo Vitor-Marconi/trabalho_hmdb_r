bb0d256ebc9ed52373787cfc35a68da7c74af0d2
Miguel Pires Gomes <119467483+miguelpiresgomes25@users.noreply.github.com>
2022-11-30 22:08:15 -0300

    Add files via upload

--- trabalho_hmdb.r
+++ trabalho_hmdb.r
@@ -1,10 +1,6 @@
-#importing and reading csv.
-
 library(readxl)
-
 structures<- read_csv("structures (1).csv")
-
-#Importing the libraries.
+#Import the libraries
 
 install.packages('dslabs')
 
@@ -31,55 +27,53 @@ library(esquisse)
 
 library(tidyr)
 
-#loading the database summum.
+#loading the database summum
 
 summary(structures)
 
-# check if there is any Na in the database.
+# check if there is any Na in the database
 
 ind <-is.na(structures)
-
 ind
+#Creates a variable for each column of the database
 
-#Creates a variable for each column of the database.
-
-#Way of representing chemical structures.
+#Way of representing chemical structures using ASCII characters
 
 smiles <- structures$Smiles
 
-#Number of atoms in the molecule.
-
-NumAtoms <- structures$NumAt
-
-#indicates molecular formula and atomic organization
+#Indicate the simple numbers of each type of atom in a molecule
 
 MolecularFormula <- structures$Molecular_Formula
 
-#The exact molecular weight of the molecule.
+#Number of atoms in the molecule
+
+NumAtoms <- structures$NumAtoms
+
+#The exact molecular weight of the molecule
 
 ExactMolWt <- structures$ExactMolWt
 
-#Number of Rotatable Bonds.
+#Number of Rotatable Bonds
 
 NumRotableBonds <- structures$NumRotatableBonds
 
-#Atom-based calculation of LogP.
+#Atom-based calculation of LogP
 
 MolLogp <- structures$MolLogP
 
-#The exact Ring Count of the molecule.
+#The exact ringcount of the molecule
 
 RingCount <- structures$Ring_Count
 
-#Number of Hydrogen Bond Acceptors.
+#Number of Hydrogen Bond Acceptors
 
 NumHAcceptors <- structures$NumHAcceptors
 
-#Topological polar surface area.
+#Topological polar surface area
 
 TPSA <- structures$TPSA
 
-#Number of Hydrogen Bond Donors.
+#Number of Hydrogen Bond Donors
 
 NumHDonors <- structures$NumHDonors
 
@@ -87,15 +81,9 @@ NumHDonors <- structures$NumHDonors
 -------------------------------------------------------------------------"
 #creating histograms 
 
-histograma<-hist(structures$NumAtoms, breaks = 10, col="darkblue", xlim=c(10,40),main="Histograma Ring Count normal curve ");
-xfit<-seq(min(structures$NumAtoms),max(structures$NumAtoms))
-yfit<-dnorm(xfit,mean=mean(structures$NumAtoms),sd=sd(structures$NumAtoms))
-yfit <- yfit*diff(histograma$mids[1:2])*length(structures$NumAtoms)
-lines(xfit, yfit, col="red", lwd=2)
+hist(RingCount, col = "darkblue", xlab="Ring Count", main = "Histogram of ring count", xlim = c(0,10))
 
-hist(RingCount, col = "purple", xlab="Ring Count", main = "Histogram of ring count", xlim = c(0,10))
-
-hist(NumAtoms, col = "darkblue", xlab = "NumAtoms", main = "histogram of NumAtoms", xlim = c(0,150))
+hist(NumAtoms, col = "purple", xlab = "NumAtoms", main = "histogram of NumAtoms", xlim = c(0,150))
 
 hist(NumHDonors, col= "red", xlab ="NumHDonors",  xlim = c(0,10))
 
@@ -103,34 +91,20 @@ hist(NumHAcceptors, col = "orange", xlab = "NumHAcceptors")
 
 hist(ExactMolWt, col = "blue", xlab = "ExactMolWt", xlim = c(0,2000))
 
+"--------------------------------------------------
+---------------------------------------------------"
 
-"---------------------------------------------------------------------
-----------------------------------------------------------------------"
 
-
-#crate graphics with the fuction plot.
-
+#crate graphics with the fuction plot
 plot(ExactMolWt,NumAtoms, col = "purple")
 plot(TPSA, NumAtoms, col= "blue")
 plot(TPSA, ExactMolWt, xlab ="Topological polar surface area", ylab = "ExactMolWt")
+"--------------------------------------------------
+---------------------------------------------------"
 
-"---------------------------------------------------------------------
-----------------------------------------------------------------------"
+#creating graphics with fuction ggplot
 
-#creating graphics with fuction ggplot.
-
-
-#Plot "ExactMolWt x NumAtoms".
-
-ggplot(structures) +
-  aes(x = NumRotableBonds, y = NumAtoms, colour = RingCount) +
-  geom_point(shape = "circle", size = 1.5) +
-  scale_color_viridis_c(option = "viridis", direction = 1) +
-  labs(title = "ExactMolWt x NumAtoms") +
-  theme_bw()
-
-#Plot 'NumRotableBonds x NumAtoms'.
-
+#Plot 'NumRotableBonds x NumAtoms', colour = RingCount
 ggplot(structures) +
   aes(x = NumRotableBonds, y = NumAtoms, colour = RingCount) +
   geom_point(shape = "circle", size = 1.5) +
@@ -139,7 +113,7 @@ ggplot(structures) +
   theme_bw()
 
 
-#Plot "RingCount x NumHAcceptors".
+#Plot RingCount x NumHAcceptors
 
 ggplot(structures) +
   aes(x = RingCount, y = NumHAcceptors, colour = NumAtoms) +
@@ -148,7 +122,7 @@ ggplot(structures) +
   labs(title = "RingCount x NumHAcceptors") +
   theme_bw()
 
-#Plot ExactmolWt x NumHAcceptors.
+#Plot ExactmolWt x NumHAcceptors 
 
 ggplot(structures) +
   aes(x = ExactMolWt, y = NumHAcceptors, colour = NumHDonors) +
@@ -158,7 +132,7 @@ ggplot(structures) +
   theme_bw()
 
 
-#plot NumrotableBonds x NumHAcceptors.
+#plot NumrotableBonds x NumHAcceptors, colors = NumHDonors
 
 ggplot(structures) +
   aes(
@@ -171,7 +145,7 @@ ggplot(structures) +
   labs(title = "NumRotableBonds x NumHAcceptors") +
   theme_minimal()
 
-#Plot NumHAcceptors x NumAtoms.
+#Plot NumHAcceptors x NumAtoms, colour = ExactMolWt
 
 ggplot(structures) +
   aes(x = NumHAcceptors, y = NumAtoms, colour = ExactMolWt) +
@@ -179,7 +153,6 @@ ggplot(structures) +
   scale_color_viridis_c(option = "viridis", direction = 1) +
   labs(title = "NumHAcceptors x NumAtoms") +
   theme_bw()
-
 "------------------------------------------------------
 -------------------------------------------------------"
 
@@ -187,34 +160,33 @@ ggplot(structures) +
 #Max, Mean, min and median of each descriptor.
 
 Max_Min_Mean_Median <- data.frame(Descriptors = c("NumAtoms", "ExactMolWt", "NumRotableBonds","RingCount","NumHAcceptors","NumHDonors","TPSA"),
-                           Max = c(max(NumAtoms),
-                                   max(ExactMolWt),
-                                   max(NumRotableBonds),
-                                   max(RingCount),
-                                   max(NumHAcceptors),
-                                   max(NumHDonors),
-                                   max(TPSA)),
-                           Min = c(min(NumAtoms),
-                                   min(ExactMolWt),
-                                   min(NumRotableBonds),
-                                   min(RingCount),
-                                   min(NumHAcceptors),
-                                   min(NumHDonors),
-                                   min(TPSA)),
-                           Mean = c(mean(NumAtoms),
-                                    mean(ExactMolWt),
-                                    mean(NumRotableBonds),
-                                    mean(RingCount),
-                                    mean(NumHAcceptors),
-                                    mean(NumHDonors),
-                                    mean(TPSA)),
-                           median = c(median(NumAtoms),
-                                      median(ExactMolWt),
-                                      median(NumRotableBonds),
-                                      median(RingCount),
-                                      median(NumHAcceptors),
-                                      median(NumHDonors),
-                                      median(TPSA)),
-                           stringsAsFactors = FALSE)
+                                  Max = c(max(NumAtoms),
+                                          max(ExactMolWt),
+                                          max(NumRotableBonds),
+                                          max(RingCount),
+                                          max(NumHAcceptors),
+                                          max(NumHDonors),
+                                          max(TPSA)),
+                                  Min = c(min(NumAtoms),
+                                          min(ExactMolWt),
+                                          min(NumRotableBonds),
+                                          min(RingCount),
+                                          min(NumHAcceptors),
+                                          min(NumHDonors),
+                                          min(TPSA)),
+                                  Mean = c(mean(NumAtoms),
+                                           mean(ExactMolWt),
+                                           mean(NumRotableBonds),
+                                           mean(RingCount),
+                                           mean(NumHAcceptors),
+                                           mean(NumHDonors),
+                                           mean(TPSA)),
+                                  median = c(median(NumAtoms),
+                                             median(ExactMolWt),
+                                             median(NumRotableBonds),
+                                             median(RingCount),
+                                             median(NumHAcceptors),
+                                             median(NumHDonors),
+                                             median(TPSA)),
+                                  stringsAsFactors = FALSE)
 Max_Min_Mean_Median
-
